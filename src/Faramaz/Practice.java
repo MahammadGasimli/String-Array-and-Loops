fed18a9a349c07791caa5c8841a7c62bba35375b
AZE <AZE@10.0.0.22>
2023-03-15 11:34:53 -0400

    Add some comment

--- src/Faramaz/Practice.java
+++ src/Faramaz/Practice.java
@@ -5,7 +5,9 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Hello yoll students");
 		System.out.println("Nice to meet you guys ");
-
+System.out.println("Hi guys");
+		
+		
 	}
 
 }
