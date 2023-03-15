bb2234e61de304e9ccd41e2cd44bd33ba61a8995
dell <dell@192.168.0.15>
2023-03-15 00:22:22 -0400

    Greetings

--- src/Nadir/Practice.java
+++ src/Nadir/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Hello yoll students");
 		System.out.println("Nice to meet you");
+		System.out.println("Hello Instructor, nice to meet you, too!");
 	}
 
 }
