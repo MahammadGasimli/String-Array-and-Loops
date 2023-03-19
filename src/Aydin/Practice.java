121ac67f23ba9a2a80dc4de3974619ad5c73edc5
aydinhamidzade <aydinhamidzade@aydins-mbp>
2023-03-19 13:35:30 -0400

    added "The big rules for Git"

--- src/Aydin/Practice.java
+++ src/Aydin/Practice.java
@@ -5,7 +5,16 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Hello yoll students");
 		System.out.println("Nice to meet you");
+//test
+//The big rules
+//	Always PULL first before you commit & push
+//	Don't push compilation errors (comment them out)
+//	Only modify your packages
+//	Don't push .gitIgnore file
+//	if you accidentally move something to 
+//  the Staging area = replace with HEAD Revision		
 
+	
 	}
 
 }
