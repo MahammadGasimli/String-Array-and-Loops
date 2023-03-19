87b762e9bffcdca8147452db2226aef6a907af6b
Baset Hamza <75188299+BasetHamza@users.noreply.github.com>
2023-03-19 13:33:55 -0400

    added arrays practice

---
+++ src/instructor/ArraysPractice.java
@@ -0,0 +1,12 @@
+package instructor;
+
+public class ArraysPractice {
+
+	public static void main(String[] args) {
+
+		// Working Directory --> Unstaged --> Staging --commit--> Head --push--> Remote Repo.
+
+		int[] nums = { 1, 2, 3 };
+	}
+
+}
