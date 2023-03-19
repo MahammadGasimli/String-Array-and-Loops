8e5cc016b80fa623be45f5840ff2ff4cc0ac4536
Baset Hamza <75188299+BasetHamza@users.noreply.github.com>
2023-03-19 13:36:10 -0400

    printed the array

--- src/instructor/ArraysPractice.java
+++ src/instructor/ArraysPractice.java
@@ -1,5 +1,7 @@
 package instructor;
 
+import java.util.Arrays;
+
 public class ArraysPractice {
 
 	public static void main(String[] args) {
@@ -7,6 +9,8 @@ public class ArraysPractice {
 		// Working Directory --> Unstaged --> Staging --commit--> Head --push--> Remote Repo.
 
 		int[] nums = { 1, 2, 3 };
+		
+		System.out.println(Arrays.toString(nums));
 	}
 
 }
