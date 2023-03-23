88f4c33e296e31379acf0ca41dbe47939986fa29
mahammadgasimli <mahammadgasimli@localhost>
2023-03-23 17:58:14 -0400

    a simple print statement.

--- src/Mahammad/Practice.java
+++ src/Mahammad/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("Hello yoll students");
 		System.out.println("Hello!");
+		System.out.println("This is a message to G4!");
 	}
 
 }
