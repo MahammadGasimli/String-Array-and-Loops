11dacc7cbc846eba7dd7fe2deee3296de73365fb
Azar <Azar@DESKTOP-M4SO71F.mynetworksettings.com>
2023-03-23 19:55:17 -0400

    practice loops

--- src/Azar/Practice.java
+++ src/Azar/Practice.java
@@ -3,9 +3,30 @@ package Azar;
 public class Practice {
 
 	public static void main(String[] args) {
-		System.out.println("Hello yoll students");
-		System.out.println("Nice to meet you");
-//test 
+		/*
+		 * Print all the values of below array in using nested for loops nested for each
+		 * loop String apps[][] = { { "Facebook", "Instagram", "Twitter", "Youtube" }, {
+		 * "Bank of America", "TD Bank", "Chase", "Wells Fargo" }, { "Hotels", "AirBnb",
+		 * "Expedia" }
+		 */
+		// nested
+		String apps[][] = { { "Facebook", "Instagram", "Twitter", "Youtube" },
+				{ "Bank of America", "TD Bank", "Chase", "Wells Fargo" }, { "Hotels", "AirBnb", "Expedia" } };
+		for (int i = 0; i < apps.length; i++) {
+			for (int j = 0; j < apps[i].length; j++) {
+				System.out.println(apps[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("-------------------------------------------------");
+
+		// for each
+		for (String[] app : apps) {
+			for (String ap : app) {
+				System.out.println(ap);
+			}
+		}
 	}
 
 }
