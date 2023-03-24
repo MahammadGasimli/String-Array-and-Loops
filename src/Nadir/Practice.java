75104db47fc0145d30546959fbffe3190b7af75c
dell <dell@192.168.0.15>
2023-03-23 20:01:45 -0400

    for loop VS for each loop
    Merge branch 'master' of https://github.com/yollinfo/C11-Group-4.git

--- src/Nadir/Practice.java
+++ src/Nadir/Practice.java
@@ -7,6 +7,25 @@ public class Practice {
 		System.out.println("Nice to meet you");
 		System.out.println("Hello Instructor, nice to meet you, too!");
 		System.out.println("Have a good day!");
+		
+		
+		String apps[][] = { 
+				 { "Facebook", "Instagram", "Twitter", "Youtube" }, 
+			                        { "Bank of America", "TD Bank", "Chase", "Wells Fargo" }, 
+			                        { "Hotels", "AirBnb", "Expedia" }
+				 };
+			
+			for (int i = 0; i < apps.length; i++) {
+				for (int j=0; j < apps[i].length; j++) {
+			
+			System.out.println(apps[i][j]);
+			}
+			}
+			
+//			for (String[] appsRow : apps) {
+//				for (String app : appsRow)
+//					System.out.println(app);		
+//			}
 	}
 
 }
