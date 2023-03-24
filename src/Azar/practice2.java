90e6e7c3e49af6552def54263736ac5abdf54282
Azar <Azar@DESKTOP-M4SO71F.mynetworksettings.com>
2023-03-23 22:03:27 -0400

    methods practice

--- src/Azar/practice2.java
+++ src/Azar/practice2.java
@@ -3,8 +3,24 @@ package Azar;
 public class practice2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		running();
+		jogging();
+		jumping();
 
 	}
 
+	public static void running() {
+		System.out.println("I'm running");
+	}
+ 
+	public static void jogging() {
+		System.out.println("I'm jogging");
+		running();
+	}
+
+	public static void jumping() {
+		System.out.println("I'm jumping");
+
+	}
 }
