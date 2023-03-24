9281c9c5af5a1553992adaa88951dd4212337f70
mahammadgasimli <mahammadgasimli@localhost>
2023-03-24 00:28:55 -0400

    Method Practise task

---
+++ src/Mahammad/MethodPractise.java
@@ -0,0 +1,21 @@
+package Mahammad;
+
+public class MethodPractise {
+
+	public static void main(String[] args) {
+		
+		running();
+		jogging();
+		jumping();
+	}
+	public static void running() {
+		System.out.println("I'm running");
+		
+	}
+	public static void jogging() {
+		System.out.println("I'm jogging");
+	}
+	public static void jumping() {
+		System.out.println("I'm jumping");
+	}
+}
